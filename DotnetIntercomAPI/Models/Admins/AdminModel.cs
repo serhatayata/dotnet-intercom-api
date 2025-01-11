a7d766c2abdb7fe9da72c9849bcdcafa192dd031
Serhat Ayata <srhtayata@gmail.com>
2025-01-11 22:27:28 +0300

    Add project files.

---
+++ DotnetIntercomAPI/Models/Admins/AdminModel.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace DotnetIntercomAPI.Models.Admins;
+
+public class AdminModel
+{
+    [JsonProperty("type")]
+    public string Type { get; set; }
+
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("email")]
+    public string Email { get; set; }
+
+    [JsonProperty("job_title")]
+    public string JobTitle { get; set; }
+
+    [JsonProperty("away_mode_enabled")]
+    public bool AwayModeEnabled { get; set; }
+
+    [JsonProperty("away_mode_reassign")]
+    public bool ArrayModeReassign { get; set; }
+
+    [JsonProperty("has_inbox_seat")]
+    public bool HasInboxSeat { get; set; }
+
+    [JsonProperty("team_ids")]
+    public List<int> TeamIds { get; set; }
+
+    [JsonProperty("avatar")]
+    public string Avatar { get; set; }
+
+    [JsonProperty("request_id")]
+    public string RequestId { get; set; }
+
+    [JsonProperty("errors")]
+    public List<ErrorModel> Errors { get; set; }
+}
---
+++ DotnetIntercomAPI/Models/ErrorModel.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace DotnetIntercomAPI.Models;
+
+public class ErrorModel
+{
+    [JsonProperty("code")]
+    public string Code { get; set; }
+
+    [JsonProperty("message")]
+    public string Message { get; set; }
+}
